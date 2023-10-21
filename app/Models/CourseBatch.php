5b199ccbf770ae2fe75b3cb1545ce99de7623aa5
Aashish0202 <ashishshinde.ukvalley@gmail.com>
2023-10-21 22:26:00 +0530

    batches

---
+++ app/Models/CourseBatch.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Course;
+use App\Models\Batch;
+
+class CourseBatch extends Model
+{
+    use HasFactory;
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function batch()
+    {
+        return $this->belongsTo(Batch::class);
+    }
+}
