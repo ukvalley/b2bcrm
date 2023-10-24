479fd8d1aebf8f2e651f701e7174a1cc83e7acfa
Ashish0202 <ashishshinde.ukvalley@gmail.com>
2023-10-24 09:19:46 +0530

    course details view

--- resources/views/recruiter/panel/courseSearch/CourseDetails.blade.php
+++ resources/views/recruiter/panel/courseSearch/CourseDetails.blade.php
@@ -1,53 +1,283 @@
-@extends('recruiter.panel.layout')
+@extends ('recruiter.panel.layout')
 
 @section('content')
 
-<!-- Begin page -->
- <!-- Begin page -->
+    <div class="main-container container">
+
+        <div class="row">
+            <div class="col-12" style="background: white;
+    padding: 6px;
+    border-radius: 8px;">
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="overview-tab" data-bs-toggle="tab" data-bs-target="#overview" type="button" role="tab" aria-controls="overview" aria-selected="true">Overview</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="requirment-tab" data-bs-toggle="tab" data-bs-target="#requirment" type="button" role="tab" aria-controls="requirment" aria-selected="false">Admission Requirment</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="dates-tab" data-bs-toggle="tab" data-bs-target="#dates" type="button" role="tab" aria-controls="dates" aria-selected="false">Course Dates</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="scholarship-tab" data-bs-toggle="tab" data-bs-target="#scholarship" type="button" role="tab" aria-controls="scholarship" aria-selected="false">Scholarship</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="about-tab" data-bs-toggle="tab" data-bs-target="#about" type="button" role="tab" aria-controls="about" aria-selected="false">About</button>
+                    </li>
+                </ul>
+
+<style>
+  .course-info{
+    padding:5px;
+
+  }
+</style>
+
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview-tab">
+                        <br>
+                            <h4>Course Particulars</h4>
+                            <hr>
+                                <div class="course-info">
+                                    <p><strong>Name</strong> <br>
+                                        Diploma in Business Fundamentals Program with Co-op Go to course page
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Course Code
+                                    </strong><br>Diploma in Business Fundamentals Program with Co-op Go to course page
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Currency</strong><br>Diploma in Business Fundamentals Program with Co-op Go to course page
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Tuition Fee
+                                    </strong><br>Diploma in Business Fundamentals Program with Co-op Go to course page
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Delivery</strong><br>Diploma in Business Fundamentals Program with Co-op Go to course page
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Duration</strong><br>Diploma in Business Fundamentals Program with Co-op Go to course page
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Application Fee
+                                    </strong><br>Diploma in Business Fundamentals Program with Co-op Go to course page
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Adventus Code
+                                    </strong><br>Diploma in Business Fundamentals Program with Co-op Go to course page
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Summary</strong><br>Diploma in Business Fundamentals Program with Co-op Go to course page
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Attendance pattern
+                                    </strong><br>Diploma in Business Fundamentals Program with Co-op Go to course page
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Language of Tuition
+
+                                    </strong><br>Diploma in Business Fundamentals Program with Co-op Go to course page
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Learning objectives / outcomes
+
+
+                                    </strong><br>Skills and knowledge in communications can enhance many marketing roles in management, strategy, PR, advertising and communications. The course has a strong emphasis on developing an understanding of how theory can be applied in practice, preparing recent graduates with an awareness of the challenges of the sector and current practitioners with the specific knowledge for application in their careers.
+
+Careers specific to branding and marketing communication can include:
+Marketing Executive
+Account Executive
+PR executive
+Brand Executive
+Marketing Communications Executive The Career and Employability Services support students in planning for their career.
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Major / Specialisation
+
+
+                                    </strong><br>Marketing Communications and Brand Management
+
+                                    </p>
+                                </div>
+
+                            </div>
+
+                            <div class="tab-pane fade" id="requirment" role="tabpanel" aria-labelledby="requirment-tab">
+
+                            <br>
+                            <h4>Admission Requirements:
+
+                            </h4>
+                            <hr>
+                                <div class="course-info">
+                                    <p><strong>Requirements</strong> <br>
+                                    Grade 12 / GED or mature student status (all transcripts must be “true certified copies” or originals). Applicants must successfully complete the admissions interview and entrance assessments with a minimum score of 20/40 in the English Assessment and a minimum score of 20/40 in the Math Assessment (with a minimum composite score of 42/80).
+
+
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Documents</strong> <br>
+                                    Grade 12 / GED or mature student status (all transcripts must be “true certified copies” or originals). Applicants must successfully complete the admissions interview and entrance assessments with a minimum score of 20/40 in the English Assessment and a minimum score of 20/40 in the Math Assessment (with a minimum composite score of 42/80).
+
+
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>English language Requirment</strong> <br>
+                                    IELTS Academic - 6.5 (minimum 6.0 in each band)
+                               TOEFL Internet-based - 87 (21 in listening and writing, 22 in speaking and 23 in reading)
+                               Pearson PTE Academic - 58
+
+                                    </p>
+                                </div>
+                               
+                            </div>
+                            <div class="tab-pane fade" id="dates" role="tabpanel" aria-labelledby="dates-tab">
+                            <br>
+                            <h4>Course Dates:
+
+                            </h4>
+                            <hr>
+                            <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Application Opens</th>
+      <th scope="col">Application Deadline</th>
+      <th scope="col">Term Start</th>
+      <th scope="col">Census Date
+</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th >--</th>
+      <td>--</td>
+      <td>--</td>
+      <td>--</td>
+    </tr>
    
-         <div class="main-container container">
+  </tbody>
+</table>
+                            </div>
 
-         <div class="row">
-            <div class="col-12">
-            <ul class="nav nav-tabs" id="myTab" role="tablist">
-  <li class="nav-item" role="presentation">
-    <button class="nav-link active" id="overview-tab" data-bs-toggle="tab" data-bs-target="#overview" type="button" role="tab" aria-controls="overview" aria-selected="true">Overview</button>
-  </li>
-  <li class="nav-item" role="presentation">
-    <button class="nav-link" id="requirment-tab" data-bs-toggle="tab" data-bs-target="#admission" type="button" role="tab" aria-controls="admission" aria-selected="false">Admission Requirment</button>
-  </li>
-  <li class="nav-item" role="presentation">
-    <button class="nav-link" id="dates-tab" data-bs-toggle="tab" data-bs-target="#dates" type="button" role="tab" aria-controls="dates" aria-selected="false">Course Dates</button>
-  </li>
-  <li class="nav-item" role="presentation">
-    <button class="nav-link" id="scholarship-tab" data-bs-toggle="tab" data-bs-target="#scholarship" type="button" role="tab" aria-controls="scholarship" aria-selected="false">Scholarship</button>
-  </li>
-</ul>
-<div class="tab-content" id="myTabContent">
-  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="overview-tab">...</div>
-  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="requirment-tab">...</div>
-  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="dates-tab">...</div>
+                            <div class="tab-pane fade" id="scholarship" role="tabpanel" aria-labelledby="scholarship-tab"><br>
+                            <h4>Scholarship</h4>
+                            <hr>
+                                <div class="course-info">
+                                    <p><strong>Application Open
+</strong> <br>
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Application Close
 
-  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="scholarship-tab">...</div>
-</div>
-</div>
-</div>
-                
+                                    </strong><br>
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>URL</strong><br><a href="https://forigen.com/dubai-u-a-e-graduate-research-scholarship-2020-2021-all-nations/   ">https://forigen.com/dubai-u-a-e-graduate-research-scholarship-2020-2021-all-nations/ </a>                                </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Value
+                                    </strong><br>The amount depends on the research grant budget. (Applicants will get some other benefits such as living allowance, health insurance, travel insurance )
+
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Eligibility</strong><br>You need to be an active graduate student at UAEU. You need to have a Good Academic Standing GPA 3.0 or 4.0. Have no other financial sponsorship.
+
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Description</strong><br>Research Assistantships in the United Arab Emirates University (UAEU) are granted to national and international Graduate Students to work on research projects under the supervision of faculty members.
+
+                                    </p>
+                                </div>
+                                </div>
+
+                                <div class="tab-pane fade" id="about" role="tabpanel" aria-labelledby="about-tab"><br>
+                            <h4>Accomodation</h4>
+                            <hr>
+                                <div class="course-info">
+                                    <p><strong>Institution overview
+</strong> <br>            Middlesex University Dubai is the first overseas campus of the renowned Middlesex University based in London, UK. The University’s first learning space in Dubai opened at Dubai Knowledge Park (DKP) in 2005 and has over 4,000 students studying from more than 118 nationalities. It is an exciting, diverse and multicultural campus that prides itself on providing a student experience with a difference. As a testament to the growth and success of its thriving international student community, the University opened a second campus location in Dubai International Academic City (DIAC) in September 2021. This is the first time an international university in Dubai is offering a multi-site campus experience across both of the emirate’s international higher educational hubs. DKP and DIAC were set up in 2003 and 2007 respectively for international institutions that promote learning. Middlesex University Dubai students, therefore, have the opportunity to participate in a much wider student community.
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Key selling points
+
+
+                                    </strong><br>Study a quality UK degree in the heart of Dubai <br>
+
+Experience a truly global university
+
+Start your studies in dubai with the option to transfer overseas
+
+Financial support through scholarships & grants
+                                    </p>
+                                </div>
+                              
+                                <div class="course-info">
+                                    <p><strong>Short name
+
+                                    </strong><br>MDX
+
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Institution type
+</strong><br>University
+
+                                    </p>
+                                </div>
+
+                                <div class="course-info">
+                                    <p><strong>University ownership
+</strong><br>Public
+
+                                    </p>
+                                </div>
+                                <div class="course-info">
+                                    <p><strong>Expected turnaround time from lodgement to offer
+</strong><br>Advised: 42 working days
+Advised data is based on information provided by the institution
+
+                                    </p>
+                                </div>
+                                </div>
+                    </div>
+                </div>
+            </div>
 
 
 
-                   
-
-
-
-                
-
-                
 
 
 
 
-</div>
 
 
-@endsection
+
+
+
+
+
+
+        </div>
+
+
+        @endsection
