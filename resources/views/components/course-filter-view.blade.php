2f2315beff7042b52369db97b06b6d96a422d5b4
ukvalley <umesh98904@gmail.com>
2023-10-26 17:02:05 +0530

    workumesh

--- resources/views/components/course-filter-view.blade.php
+++ resources/views/components/course-filter-view.blade.php
@@ -59,11 +59,14 @@
 
 
                 <div class="row mt-3">
-                    <div class="col-12 card bg-info" >
+                    <div class="col-12 card bg-gray" >
                         
                     <div class="card-header">
                         <a href="{{url('/')}}/agent/CourseDetails/{{$course->id}}">
-                        <i class="bi bi-book" style="margin-right: 5px;"></i>{{$course->name}}
+
+                        <i class="bi bi-book" style="margin-right: 5px;"></i>
+
+                        {{$course->name}}
 
                         </a>
 
@@ -75,9 +78,10 @@
                         <input type="hidden" name="student_id" value="{{ $student->id }}">
                         <input type="hidden" name="course_id" value="{{ $course->id }}">
                         @if ($student->shortlistedCourses->contains($course->id))
-                       <button class="btn btn-outline-danger" type="submit">  <i class="bi bi-heart"></i> Add to Shortlist </button>
-                       @else
                        <button class="btn btn-outline-warning" type="submit">  <i class="bi bi-heart-fill"></i> Remove from Shortlist </button>
+                       @else
+                    
+                       <button class="btn btn-outline-danger" type="submit">  <i class="bi bi-heart"></i> Add to Shortlist </button>
                        @endif
 
                      </form>
@@ -93,7 +97,7 @@
 
                     <div class="row  bg-light text-dark">
                     <div class="col-md-3 bg-light d-flex  align-items-center">
-                        <img  src="https://civs.online/wp-content/uploads/2023/01/CIVS-White-01-1024x285.png" alt="Logo" style="width: 100%; height: 60%; padding:5px">
+                        <img  src="{{asset('image/course/'. $course->institution->logo)}}" alt="Logo" style="width: 100%; height: 60%; padding:5px">
                     </div> 
 
                     <div class="col-md-9">
