706a87545e2848c4433ac6697c43da0f8a9799da
Dhaval <80376340+dpdhavalpatel@users.noreply.github.com>
2024-01-12 11:45:02 +0530

    notification change

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -3,24 +3,17 @@
 namespace App\Http\Controllers;
 use App\Models\Notification;
 use App\Models\User;
-use DB;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
-    public function index()
-    {
-       $admin = User::where('user_type_id','4')->first();
-       
-       if($admin){
-            $adminid = $admin->id;
-            //Retrieve and show notifications in the admin panel
-            $notifications =Notification::where('user_id', $adminid)->get();
-
-            return view('notifications.index', compact('notifications'));
-        }
-          
-          return view('notifications.index');
-    }
+	public function index()
+	{
+		$id= Auth::user()->id;
+		//Retrieve and show notifications in the admin panel
+		$notifications =Notification::where('user_id', $id)->get();
+		return view('notifications.index', compact('notifications'));
+	}
 }
