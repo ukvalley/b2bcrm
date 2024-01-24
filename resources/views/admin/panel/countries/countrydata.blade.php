e46a37fbaac802f1ad7b8bf773057210ffd492c7
Dhaval <dhavalpatel2193@gmail.com>
2024-01-24 16:06:57 +0530

    datatable

--- resources/views/admin/panel/countries/countrydata.blade.php
+++ resources/views/admin/panel/countries/countrydata.blade.php
@@ -1,48 +1,48 @@
 @extends('admin.panel.layout')
-@section('content') 
+@section('content')
 
 
-        <!-- Header ends -->
+<!-- Header ends -->
 
-        <!-- main page content -->
-        <div class="main-container container">
-            <!-- welcome user -->
-           
+<!-- main page content -->
+<div class="main-container container">
+    <!-- welcome user -->
 
-            
 
-            <!-- connection -->
-           
-            
-            <!-- Dark mode switch -->
-            <div class="row mb-4">
-                <div class="col-12">
-                    <div class="card shadow-sm">
-                        <div class="card-header">
-                            <h3>{{$countryData->country_name}}</h3>
-                        </div>
-                    </div>
+
+
+    <!-- connection -->
+
+
+    <!-- Dark mode switch -->
+    <div class="row mb-4">
+        <div class="col-12">
+            <div class="card shadow-sm">
+                <div class="card-header">
+                    <h3>{{$countryData->country_name}}</h3>
                 </div>
             </div>
+        </div>
+    </div>
 
 
-            <div class="row mb-4">
-                <div class="col-12">
-                    <div class="card shadow-sm">
-                        <div class="card-body">
+    <div class="row mb-4">
+        <div class="col-12">
+            <div class="card shadow-sm">
+                <div class="card-body">
 
-                            <div class="row">
-                                <img src="{{ asset('images/country_header_image/' . $countryData->country_header_image) }}">
-                            </div>
-                            <div class="row mt-5">
-                            <div class="col-md-8">
+                    <div class="row">
+                        <img src="{{ asset('images/country_header_image/' . $countryData->country_header_image) }}">
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-md-8">
 
-                                {!! $countryData->information_data !!}
-   
-                                
-                            </div>
-                            <div class="col-md-4">
-                                <div class="row mb-5">
+                            {!! $countryData->information_data !!}
+
+
+                        </div>
+                        <div class="col-md-4">
+                            <div class="row mb-5">
                                 <h3 class="mb-3">News</h3>
                                 @if(count($countryData->news) != 0 || $countryData->news != null)
                                 @foreach($countryData->news as $news_data)
@@ -57,58 +57,55 @@
                                 <img width="100%" src="https://img.freepik.com/free-vector/no-data-concept-illustration_114360-536.jpg?w=740&t=st=1697628862~exp=1697629462~hmac=bf41dafc7537571f92dc5671115d9f47492f340de88407f047936efe4354cedd">
                                 @endif
                             </div>
-                            
-                           
+
+
 
 
                             <div class="row mb-5">
                                 <h3 class="mb-3">Video</h3>
-                                
+
                                 <div>
 
                                     @if(isset($countryData->youtube_link))
 
 
-                                      <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary video-btn" data-toggle="modal" data-src="https://player.vimeo.com/video/58385453?badge=0&autoplay=1&loop=1" data-target="#myModal">
-  Play Vimeo Video
-</button>
+                                    <!-- Button trigger modal -->
+                                    <button type="button" class="btn btn-primary video-btn" data-toggle="modal" data-src="https://player.vimeo.com/video/58385453?badge=0&autoplay=1&loop=1" data-target="#myModal">
+                                        Play Vimeo Video
+                                    </button>
 
-<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
 
-      
-      <div class="modal-body">
 
-       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>        
-        <!-- 16:9 aspect ratio -->
-<div class="embed-responsive embed-responsive-16by9">
-  <iframe class="embed-responsive-item" src="" id="video"  allowscriptaccess="always" allow="autoplay"></iframe>
-</div>
-        
-        
-      </div>
+                                                <div class="modal-body">
 
-    </div>
-  </div>
-</div> 
-                                    
-                                    
-                                    
-                                   
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                    <!-- 16:9 aspect ratio -->
+                                                    <div class="embed-responsive embed-responsive-16by9">
+                                                        <iframe class="embed-responsive-item" src="" id="video" allowscriptaccess="always" allow="autoplay"></iframe>
+                                                    </div>
+
+
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+
 
                                     {!! $countryData->youtube_link !!}
 
                                     @else
                                     <img width="100%" src="https://app.adventus.io/img/blank-news.svg">
                                     @endif
-                                   
+
                                 </div>
-                                
+
                             </div>
 
 
@@ -118,11 +115,11 @@
                                 @foreach($countryData->links as $links_data)
                                 <div>
 
-                                    <li>  <a style="color: green;" href="{{$links_data->url}}">
-                                        <i class="bi bi-box-arrow-up-right"></i>
-                                        {{$links_data->title}} 
-                                    </a> </li> 
-                                    
+                                    <li> <a style="color: green;" href="{{$links_data->url}}">
+                                            <i class="bi bi-box-arrow-up-right"></i>
+                                            {{$links_data->title}}
+                                        </a> </li>
+
                                     <hr>
                                 </div>
                                 @endforeach
@@ -131,27 +128,20 @@
                                 @endif
                             </div>
 
-                            
 
-                            </div>
+
                         </div>
                     </div>
                 </div>
             </div>
-
-            
-
-           
-
         </div>
-        <!-- main page content ends -->
+
+
+    </div>
+    <!-- main page content ends -->
 
 
     </main>
     <!-- Page ends-->
 
-
-
-
-
-@endsection
+    @endsection
